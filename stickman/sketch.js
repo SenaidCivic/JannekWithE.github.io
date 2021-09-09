5de40466d9175ac8c4078048c8ded469c53b27d2
Jannek <jannekNY@gmail.com>
2021-09-09 13:44:51 +0200

    Dette er min cricle som ikke er en "stickman"

---
+++ stickman/sketch.js
@@ -0,0 +1,23 @@
+let XD = 69
+const Mid = 200
+
+
+function setup() {
+  fill(52, 219, 235)
+  createCanvas(400, 400);
+}
+
+
+function draw() {
+  background(220);
+  
+  let uwu=mouseX
+  let MY=mouseY
+  
+  noStroke()
+  fill(uwu,MY,XD)
+  circle(uwu,MY,XD);
+  
+  
+
+}
